c94f224f7c79111b9629e6d10abf1819e323e653
chat5cv51dgrh <chat5cv51dgrh@gmail.com>
2024-06-06 19:00:53 -0600

    Commit

--- js/init.js
+++ js/init.js
@@ -9,10 +9,16 @@
 // @ts-ignore
 firebase.initializeApp({
     apiKey: "AIzaSyCewItBbDwJ5DQpiT84NqSmIE0gJk_xc7E",
+
     authDomain: "chatoncloud.firebaseapp.com",
+  
     projectId: "chatoncloud",
+  
     storageBucket: "chatoncloud.appspot.com",
+  
     messagingSenderId: "397146642025",
+  
     appId: "1:397146642025:web:834fedd5de470359ec74f8",
+  
     measurementId: "G-SYPV4PFSXQ"
 });
