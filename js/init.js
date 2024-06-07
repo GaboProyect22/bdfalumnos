e59c9c923a8af8636dcd7a043f04ca5558eedd2e
chat5cv51dgrh <chat5cv51dgrh@gmail.com>
2024-06-06 18:46:09 -0600

    Correctivo

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCewItBbDwJ5DQpiT84NqSmIE0gJk_xc7E",
+    authDomain: "chatoncloud.firebaseapp.com",
+    projectId: "chatoncloud",
+    storageBucket: "chatoncloud.appspot.com",
+    messagingSenderId: "397146642025",
+    appId: "1:397146642025:web:834fedd5de470359ec74f8",
+    measurementId: "G-SYPV4PFSXQ"
+});
